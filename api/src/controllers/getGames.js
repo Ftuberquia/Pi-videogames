39b6dec9069139549a25a0e03d5dbb81af7be399
Ftuberquia <frank.tuberquiarojas@gmail.com>
2023-06-24 12:05:39 -0500

    first commit

---
+++ api/src/controllers/getGames.js
@@ -0,0 +1,15 @@
+const { dbInfo, apiVideogames } = require('../utils');
+
+
+const gamesSaved = async () => {
+    const apiGames = await apiVideogames();
+    const dbVideogames = await dbInfo();
+    const allGames = apiGames.concat(dbVideogames);
+    return allGames
+};   
+
+
+
+module.exports={
+    gamesSaved
+};
---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Landing from './views/Landing/Landing.jsx';
+import Home from './views/Home/Home.jsx';
+import GameDetail from './Components/GameDetail/GameDetail';
+import Form from './views/Form/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Henry Videogames</h1>
+    </div>
+  );
+}
+
+export default App;
